2b989b5572ed7729b89707fb3fc67c198f508b78
Luis Bacelar <lmbacelar@gmail.com>
2015-12-02 00:56:38 +0000

    added user_email virtual attr_accessor. Added import from CSV, XLSX via roo

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -19,4 +19,33 @@ class Instrument < ActiveRecord::Base
       order 'created_at DESC'
     end
   end
+
+  def user_email
+    user.email
+  end
+
+  def user_email= email
+    self.user = User.find_by email: email
+  end
+
+  IMPORT_ATTRS = %w{reference designation manufacturer model part_number serial_number remarks user_email}
+  def self.import file
+    sheet = open_sheet file
+    header = sheet.row 1
+    (2..sheet.last_row).each do |i|
+      row = Hash[[header, sheet.row(i)].transpose]
+      attrs = Hash[row.to_hash.slice(*IMPORT_ATTRS).map{ |k,v| [ k, v || '' ] }]
+      instr = find_by(reference: row['reference']) || new
+      instr.update_attributes attrs
+      instr.save!
+    end
+  end
+
+  def self.open_sheet file
+    case File.extname file.to_path
+      when '.csv'  then Roo::CSV.new    file.to_path, file_warnings: :ignore
+      when '.xlsx' then Roo::Excelx.new file.to_path, file_warnings: :ignore
+      else raise "Unknown file type: #{file.to_path}"
+    end
+  end
 end
