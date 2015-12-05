6878a0706276fc2e81e4459d004808d09f430b50
Luis Bacelar <lmbacelar@gmail.com>
2015-12-05 23:56:35 +0000

    added import_instruments route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :instruments
+  resources :instruments do
+    collection { post :import }
+  end
   devise_for :users
   root to: 'instruments#index'
 end
