d21cc3f472f5e34b7ad0e4c147d4075159feceec
Luis Bacelar <lmbacelar@gmail.com>
2015-12-08 20:05:57 +0000

    added department presenter

---
+++ app/presenters/department_presenter.rb
@@ -0,0 +1,26 @@
+class DepartmentPresenter < BasePresenter
+  presents :department
+  delegate :designation, to: :department
+
+  def name
+    if h.policy(department).show?
+      h.link_to department.name, department
+    else
+      department.name
+    end
+  end
+
+  def edit_link
+    if h.policy(department).edit?
+      h.link_to 'Edit', h.edit_department_path(department), class: :default
+    end
+  end
+
+  def destroy_link
+    if h.policy(department).destroy?
+      h.link_to 'Destroy', department, method: :delete,
+                                       data: { confirm: 'Are you sure?' },
+                                       class: :danger
+    end
+  end
+end
