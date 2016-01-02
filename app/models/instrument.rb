928c59ae2f0a91e73c6ec9e8e5d4b53fb1d7d91d
Luis Bacelar <lmbacelar@gmail.com>
2016-01-02 14:54:53 +0000

    added manufacturer relation, manufacturer_name virtual attributes, updated search to reference associated manufacturer name attribute

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,21 +1,23 @@
 class Instrument < ActiveRecord::Base
 
   belongs_to :department
+  belongs_to :manufacturer
 
-  validates :reference,   presence: true, uniqueness: true
-  validates :designation, presence: true
-  validates :department,  presence: true
+  validates :reference,     presence: true, uniqueness: true
+  validates :designation,   presence: true
+  validates :manufacturer,  presence: true
+  validates :department,    presence: true
 
   include Serializable
-  io_attributes :reference,:designation, :manufacturer, :model, :part_number, :serial_number, :remarks, :department_name
+  io_attributes :reference,:designation, :manufacturer_name, :model, :part_number, :serial_number, :remarks, :department_name
 
   extend FriendlyId
   friendly_id :reference, use: :slugged
 
   include PgSearch
   pg_search_scope :full_text_search,
-                     against: [ :reference, :designation, :manufacturer, :model, :serial_number ],
-                     associated_against: { department: [:name] },
+                     against: [ :reference, :designation, :model, :serial_number ],
+                     associated_against: { manufacturer: [:name], department: [:name] },
                      using: { tsearch: { dictionary: 'english', prefix: true } }
   def self.search query
     if query.present?
@@ -23,6 +25,14 @@ class Instrument < ActiveRecord::Base
     else
       order 'created_at DESC'
     end
+  end
+
+  def manufacturer_name
+    manufacturer.try :name
+  end
+
+  def manufacturer_name= name
+    self.manufacturer = Manufacturer.where(name: name).first_or_create if name
   end
 
   def department_name
