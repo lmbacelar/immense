bc7cf140f345174faadf341e57cd60d4758ae2ec
Luis Bacelar <lmbacelar@gmail.com>
2016-01-02 22:48:12 +0000

    prioritized Department autocomplete route. added Model autocomplete route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
+  get 'departments/autocomplete',   to: 'departments#autocomplete'
   resources :departments do
     collection do
       post :import
     end
   end
 
-  get 'departments/autocomplete',   to: 'departments#autocomplete'
   get 'manufacturers/autocomplete', to: 'manufacturers#autocomplete'
-
+  get 'models/autocomplete',        to: 'models#autocomplete'
   resources :instruments do
     collection { post :import }
   end
