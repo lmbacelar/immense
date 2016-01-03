a00cb7556d9c0ee992658330b184376ada996b32
Luis Bacelar <lmbacelar@gmail.com>
2016-01-03 00:23:00 +0000

    removed extra whitespace

--- config/initializers/pg_search.rb
+++ config/initializers/pg_search.rb
@@ -1,7 +1,7 @@
-PgSearch.multisearch_options = { 
-  using: { 
-    tsearch: { 
-      dictionary: 'english', 
-      prefix: true } 
-  } 
+PgSearch.multisearch_options = {
+  using: {
+    tsearch: {
+      dictionary: 'english',
+      prefix: true }
+  }
 }
