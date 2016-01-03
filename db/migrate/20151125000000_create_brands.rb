b26b8e9ce738d11b684a74b57d3206bf9b86a2ac
Luis Bacelar <lmbacelar@gmail.com>
2016-01-03 14:09:50 +0000

    removed extra whitespace

--- db/migrate/20151125000000_create_brands.rb
+++ db/migrate/20151125000000_create_brands.rb
@@ -1,7 +1,7 @@
 class CreateBrands < ActiveRecord::Migration
   def change
     create_table :brands do |t|
-      t.string  :name,        null: false, default: ''
+      t.string  :name, null: false, default: ''
 
       t.timestamps null: false
 
