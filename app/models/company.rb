f20709ba95028c06266027e14de19b63c8812e49
Luis Bacelar <lmbacelar@gmail.com>
2016-01-05 02:21:09 +0000

    added Company model, set required and unique validations, serializable attributes, slugged friendly_ids and search capabilities

---
+++ app/models/company.rb
@@ -0,0 +1,23 @@
+class Company < ActiveRecord::Base
+  validates :short_name, presence: true, uniqueness: true
+  validates :name,       presence: true, uniqueness: true
+  validates :vat_number, presence: true, uniqueness: true
+  validates :vat_prefix, presence: true
+
+  include Serializable
+  io_attributes :short_name, :name, :vat_number, :vat_prefix
+
+  extend FriendlyId
+  friendly_id :short_name, use: :slugged
+
+  def self.search query
+    if query.present?
+      where "vat_number = '#{query}' OR
+             vat_prefix = '#{query}' OR
+             short_name ilike '%#{query}' OR
+             name ilike '%#{query}%'"
+    else
+      all
+    end
+  end
+end
