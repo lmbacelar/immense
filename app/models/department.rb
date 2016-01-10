0263111688bcb8fff4a705b6434d8ffce98f2252
Luis Bacelar <lmbacelar@gmail.com>
2016-01-10 17:44:15 +0000

    refactored using ObjectBelongable concern. Setting department_object with virtual attribute instead of before_save callback

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,14 +2,14 @@ class Department < ActiveRecord::Base
 
   SEPARATOR = '/'
 
-  belongs_to :company_object, class_name: 'Company', foreign_key: :company_id
+  include ObjectBelongable
+  belongs_to_object :company
   belongs_to :parent,   class_name: 'Department'
   has_many   :children, class_name: 'Department', foreign_key: :parent_id
   has_many   :instruments
 
   validates :name, uniqueness: true
   validate  :parent_existence
-  before_save :set_parent
 
   default_scope { order 'name ASC' }
 
@@ -27,16 +27,16 @@ class Department < ActiveRecord::Base
     end
   end
 
+  def company= value
+    self.company_object = Company.find_by_short_name value
+  end
+
   def short_name
     name.split(SEPARATOR).last
   end
 
   def parent_name
     name.split(SEPARATOR)[0..-2].join(SEPARATOR)
-  end
-
-  def company
-    company_object.to_s
   end
 
   def to_s
@@ -51,9 +51,5 @@ class Department < ActiveRecord::Base
           errors.add :name, "parent department '#{parent_name}' must exist"
         end
       end
-    end
-
-    def set_parent
-      self.parent = @parent
     end
 end
