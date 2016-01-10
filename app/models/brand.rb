0e8ad5d2deb9fa2e7d1b61b1f74d6a1ae91cd595
Luis Bacelar <lmbacelar@gmail.com>
2016-01-10 15:54:32 +0000

    added to_s method

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -3,4 +3,8 @@ class Brand < ActiveRecord::Base
   has_many :models
 
   validates :name, uniqueness: true
+
+  def to_s
+    name
+  end
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,6 +1,10 @@
 class Model < ActiveRecord::Base
   belongs_to :brand_object, class_name: 'Brand', foreign_key: :brand_id
 
+  def to_s
+    name
+  end
+
   def brand
     brand_object&.name
   end
