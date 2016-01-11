113fe33ba7e67058ec46bbf7a5f74f59ead7bfd4
Luis Bacelar <lmbacelar@gmail.com>
2016-01-11 13:56:10 +0000

    param renaming on model autocomplete

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,6 +1,6 @@
 class ModelsController < ApplicationController
   def autocomplete
-    brand = Brand.find_by name: params[:brand_name]
+    brand = Brand.find_by name: params[:brand]
     models = brand.models.where("name ilike '%#{params[:term]}%'").order(:name)
     authorize models
     render json: models.pluck(:name)
