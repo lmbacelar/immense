418d2dddb30c00a74bc886acd7b03987d0ee3424
Luis Bacelar <lmbacelar@gmail.com>
2016-01-11 13:57:55 +0000

    added instruments association, company delegated to department_object

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   include ObjectBelongable
   belongs_to_object :department
+  has_many :instruments, through: :department_object
+
+  delegate :company, to: :department_object
 
   def department= name
     self.department_object = Department.find_by_name name
